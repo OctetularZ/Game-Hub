664f30aaaf71f3ac3d427a03fc45ff8fca1c1829
OctetularZ <taiabayomi15@gmail.com>
2025-08-05 18:31:51 +0100

    Show spinner when fetching gneres. Will replace with Skeleton

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,14 @@
 import React from "react"
 import useGenres from "../hooks/useGenres"
-import { HStack, Image, List, Text } from "@chakra-ui/react"
+import { HStack, Image, List, Spinner, Text } from "@chakra-ui/react"
 import getCroppedImageUrl from "../services/image-url"
 
 const GenreList = () => {
-  const { data } = useGenres()
+  const { data, isLoading, error } = useGenres()
+
+  if (error) return null
+
+  if (isLoading) return <Spinner />
   return (
     <List.Root unstyled={true}>
       {data.map((genre) => (
